2cc4e177ec19cad12a6f97925ffd5bb94927c40c
prembalaji39 <prembalaji@accolitedigital.com>
2022-05-05 15:10:19 +0530

    PowerCharts Fix

--- src/Components/PowerChart_XTSuite/PowerCharts_XT_Suite.jsx
+++ src/Components/PowerChart_XTSuite/PowerCharts_XT_Suite.jsx
@@ -8,6 +8,15 @@ import RenderStockCharts from "./StockCharts";
 import RenderSunBurstCharts from "./SunBursts";
 import RenderWaterfallCharts from "./Waterfall";
 
+import FusionCharts from "fusioncharts";
+import PowerCharts from "fusioncharts/powercharts";
+import TreeMap from "fusioncharts/treemap";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, PowerCharts, FusionTheme);
+ReactFC.fcRoot(FusionCharts, TreeMap, FusionTheme);
+
 function PowerChartsXTSuite(props) {
   return (
     <>
